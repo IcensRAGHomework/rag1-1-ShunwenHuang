9f7f2985bdd245f66c066cb52c42b13f5df16e0e
ShunwenHuang <shunwen_huang@asus.com>
2025-01-23 19:35:08 +0800

    Update student_assignment.py

--- student_assignment.py
+++ student_assignment.py
@@ -9,7 +9,55 @@ from langchain_core.messages import HumanMessage
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
 
+
+# import homework 1
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate
+
+import re
+from langchain_core.output_parsers import JsonOutputParser
+from pydantic import BaseModel, Field
+from typing import List
+
+
+class Holiday_Item(BaseModel):
+    """Information about a holiday."""
+    date: str = Field(..., description="Date of holiday")
+    name: str = Field(..., description="Name of holiday.")
+
+
+class Fianl_Result(BaseModel):
+    """Identifying information about all holidays in specific month."""
+    Result: List[Holiday_Item]
+
+# Custom parser
+def extract_json(message: AIMessage) -> List[dict]:
+    """Extracts JSON content from a string where JSON is embedded between \`\`\`json and \`\`\` tags.
+
+    Parameters:
+        text (str): The text containing the JSON content.
+
+    Returns:
+        list: A list of extracted JSON strings.
+    """
+    text = message.content
+    # Define the regular expression pattern to match JSON blocks
+    pattern = r"\`\`\`json(.*?)\`\`\`"
+
+    # Find all non-overlapping matches of the pattern in the string
+    matches = re.findall(pattern, text, re.DOTALL)
+
+    # Return the list of matched JSON strings, stripping any leading or trailing whitespace
+    try:
+        return [json.loads(match.strip()) for match in matches]
+    except Exception:
+        raise ValueError(f"Failed to parse: {message}")
+# import homwork 1
+
+
 def generate_hw01(question):
+
+# import homework 1
 llm = AzureChatOpenAI(
             model=gpt_config['model_name'],
             deployment_name=gpt_config['deployment_name'],
@@ -46,6 +94,8 @@ prompt = ChatPromptTemplate.from_messages(
     # convert to json format
     return json.dumps(response, ensure_ascii = False)
     #return response
+
+# import homework 1
 
 def generate_hw02(question):
     pass
